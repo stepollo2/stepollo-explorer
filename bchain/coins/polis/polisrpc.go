4c2055b96998d0f8b17f4bbc204b51fb6089f600
Enrique <eabz@polispay.org>
2020-03-17 12:02:17 -0600

    Remove test data and fix Polis data

--- bchain/coins/polis/polisrpc.go
+++ bchain/coins/polis/polisrpc.go
@@ -2,56 +2,87 @@ package polis
 
 import (
 	"encoding/json"
+	"github.com/golang/glog"
 	"github.com/grupokindynos/coins-explorer/bchain"
 	"github.com/grupokindynos/coins-explorer/bchain/coins/btc"
-
-	"github.com/golang/glog"
+	"github.com/juju/errors"
 )
 
-// PolisRPC is an interface to JSON-RPC bitcoind service.
 type PolisRPC struct {
 	*btc.BitcoinRPC
 }
 
-// NewPolisRPC returns new PolisRPC instance.
+const firstBlockWithSpecialTransactions = 454000
+
 func NewPolisRPC(config json.RawMessage, pushHandler func(bchain.NotificationType)) (bchain.BlockChain, error) {
 	b, err := btc.NewBitcoinRPC(config, pushHandler)
 	if err != nil {
 		return nil, err
 	}
-
 	s := &PolisRPC{
 		b.(*btc.BitcoinRPC),
 	}
 	s.RPCMarshaler = btc.JSONMarshalerV1{}
 	s.ChainConfig.SupportsEstimateSmartFee = false
-
 	return s, nil
 }
 
-// Initialize initializes PolisRPC instance.
 func (b *PolisRPC) Initialize() error {
 	ci, err := b.GetChainInfo()
 	if err != nil {
 		return err
 	}
 	chainName := ci.Chain
-
 	params := GetChainParams(chainName)
-
-	// always create parser
 	b.Parser = NewPolisParser(params, b.ChainConfig)
-
-	// parameters for getInfo request
-	if params.Net == MainnetMagic {
-		b.Testnet = false
-		b.Network = "livenet"
-	} else {
-		b.Testnet = true
-		b.Network = "testnet"
-	}
-
+	b.Testnet = false
+	b.Network = "livenet"
 	glog.Info("rpc: block chain ", params.Name)
-
 	return nil
+}
+
+func (b *PolisRPC) GetBlock(hash string, height uint32) (*bchain.Block, error) {
+	if hash == "" && height < firstBlockWithSpecialTransactions {
+		return b.BitcoinRPC.GetBlock(hash, height)
+	}
+	var err error
+	if hash == "" && height > 0 {
+		hash, err = b.GetBlockHash(height)
+		if err != nil {
+			return nil, err
+		}
+	}
+	glog.V(1).Info("rpc: getblock (verbosity=1) ", hash)
+	res := btc.ResGetBlockThin{}
+	req := btc.CmdGetBlock{Method: "getblock"}
+	req.Params.BlockHash = hash
+	req.Params.Verbosity = 1
+	err = b.Call(&req, &res)
+	if err != nil {
+		return nil, errors.Annotatef(err, "hash %v", hash)
+	}
+	if res.Error != nil {
+		return nil, errors.Annotatef(res.Error, "hash %v", hash)
+	}
+	txs := make([]bchain.Tx, 0, len(res.Result.Txids))
+	for _, txid := range res.Result.Txids {
+		tx, err := b.GetTransaction(txid)
+		if err != nil {
+			if err == bchain.ErrTxNotFound {
+				glog.Errorf("rpc: getblock: skipping transanction in block %s due error: %s", hash, err)
+				continue
+			}
+			return nil, err
+		}
+		txs = append(txs, *tx)
+	}
+	block := &bchain.Block{
+		BlockHeader: res.Result.BlockHeader,
+		Txs:         txs,
+	}
+	return block, nil
+}
+
+func (b *PolisRPC) GetTransactionForMempool(txid string) (*bchain.Tx, error) {
+	return b.GetTransaction(txid)
 }
