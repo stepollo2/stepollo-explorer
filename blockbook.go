fa1cad95889b4fdcb6c48f3cd02c35182fd74602
Enrique <eabz@polispay.org>
2020-03-17 12:24:22 -0600

    Fix

--- blockbook.go
+++ blockbook.go
@@ -172,7 +172,7 @@ func mainWithExitCode() int {
 		return exitCodeFatal
 	}
 
-	index, err = db.NewRocksDB(*dbPath, *dbCache, *dbMaxOpenFiles, chain.GetChainParser(), metrics)
+	index, err = db.NewRocksDB(*dbPath, uint64(*dbCache), *dbMaxOpenFiles, chain.GetChainParser(), metrics)
 	if err != nil {
 		glog.Error("rocksDB: ", err)
 		return exitCodeFatal
