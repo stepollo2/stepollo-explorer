e5b8d7930d33866fa58715dba842bf48042a83b7
stepollo2 <56636447+stepollo2@users.noreply.github.com>
2020-05-02 12:46:54 +0200

    Create bitgreenrpc.go

---
+++ bchain/coins/bitg/bitgreenrpc.go
@@ -0,0 +1,54 @@
+package bitg
+
+import (
+	"encoding/json"
+	"github.com/stepollo2/stepollo-explorerbchain"
+	"github.com/stepollo2/stepollo-explorerbchain/coins/btc"
+
+	"github.com/golang/glog"
+)
+
+// BitGreenRPC is an interface to JSON-RPC bitcoind service.
+type BitgreenRPC struct {
+	*btc.BitcoinRPC
+}
+
+// NewBitgreenRPC returns new BitGreenRPC instance.
+func NewBitgreenRPC(config json.RawMessage, pushHandler func(bchain.NotificationType)) (bchain.BlockChain, error) {
+	b, err := btc.NewBitcoinRPC(config, pushHandler)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &BitgreenRPC{
+		b.(*btc.BitcoinRPC),
+	}
+
+	return s, nil
+}
+
+// Initialize initializes BGoldRPC instance.
+func (b *BitgreenRPC) Initialize() error {
+	ci, err := b.GetChainInfo()
+	if err != nil {
+		return err
+	}
+	chainName := ci.Chain
+
+	params := GetChainParams(chainName)
+
+	// always create parser
+	b.Parser = NewBitgreenParser(params, b.ChainConfig)
+
+	// parameters for getInfo request
+	if params.Net == MainnetMagic {
+		b.Testnet = false
+		b.Network = "livenet"
+	} else {
+		b.Testnet = true
+		b.Network = "testnet"
+	}
+
+	glog.Info("rpc: block chain ", params.Name)
+	return nil
+}
